0d12b18f763da65159525897fc4dd176eaae16a1
Jimmy Carlson <carlso70@purdue.edu>
2016-07-13 13:24:29 -0500

    Added fromJSON methods to decode json objects into the data models(BoxOfficeMovies)

--- app/src/main/java/com/example/panth/rottentomatoes/BoxOfficeMovie.java
+++ app/src/main/java/com/example/panth/rottentomatoes/BoxOfficeMovie.java
@@ -2,7 +2,12 @@ package com.example.panth.rottentomatoes;
 
 import android.text.TextUtils;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.jar.Pack200;
 
 /**
  * Created by Jimmy Carlson on 7/13/2016.
@@ -47,5 +52,55 @@ public class BoxOfficeMovie {
         return title;
     }
 
+    // Returns a BoxOfficeMovie given the expected JSON
+    // Stores title, year, synopsis, posterURL, critic score, and cast list
+    public static BoxOfficeMovie fromJson(JSONObject jsonObject)
+    {
+        BoxOfficeMovie boxOfficeMovie = new BoxOfficeMovie();
+        try {
+            boxOfficeMovie.title = jsonObject.getString("title");
+            boxOfficeMovie.year = jsonObject.getInt("year");
+            boxOfficeMovie.synopsis = jsonObject.getString("synopsis");
+            boxOfficeMovie.posterURL = jsonObject.getJSONObject("posters").getString("thumbnail");
+            boxOfficeMovie.criticScore = jsonObject.getJSONObject("ratings").getInt("critics_score");
+            boxOfficeMovie.castList = new ArrayList<String>();
+            JSONArray abridgedCast = jsonObject.getJSONArray("abridged_cast");
+            for (int i = 0; i < abridgedCast.length(); i++)
+            {
+                boxOfficeMovie.castList.add(abridgedCast.getJSONObject(i).getString("name"));
+            }
+        }catch (JSONException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        return boxOfficeMovie;
+    }
+
+    // Transforms array of box office movie json results into the BoxOfficeMovies
+    public static ArrayList<BoxOfficeMovie> fromJson(JSONArray jsonArray)
+    {
+        ArrayList<BoxOfficeMovie> movies = new ArrayList<BoxOfficeMovie>(jsonArray.length());
+        // Each result in json array, decode and convert to a BoxOfficeMovie
+        for (int i = 0; i < jsonArray.length(); i++)
+        {
+            JSONObject moviesJSON = null;
+            try {
+                moviesJSON = jsonArray.getJSONObject(i);
+            }catch (Exception e)
+            {
+                e.printStackTrace();
+                continue;
+            }
+
+            BoxOfficeMovie movie = BoxOfficeMovie.fromJson(moviesJSON);
+            if (movie != null)
+            {
+                movies.add(movie);
+            }
+        }
+
+        return movies;
+    }
 
 }
