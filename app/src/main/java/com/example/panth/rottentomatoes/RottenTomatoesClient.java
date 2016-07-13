7a12d8732d35bd5e5599fa055fe457f2a1b50c58
carlso70 <carlso70@purdue.edu>
2016-07-12 23:30:34 -0500

    Add RT class

---
+++ app/src/main/java/com/example/panth/rottentomatoes/RottenTomatoesClient.java
@@ -0,0 +1,32 @@
+package com.example.panth.rottentomatoes;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by Jimmy Carlson on 7/12/2016.
+ */
+public class RottenTomatoesClient {
+    private final String API_KEY = "...getkey...";
+    private final String API_BASE_URL = "http://api.rottentomatoes.com/api/public/v1.0/";
+    private AsyncHttpClient client;
+
+    public RottenTomatoesClient()
+    {
+        this.client = new AsyncHttpClient();
+    }
+
+    private String getApiUrl(String relativeUrl)
+    {
+        return API_BASE_URL + relativeUrl;
+    }
+
+    // http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=<key>
+    public void getBoxOfficeMovies(JsonHttpResponseHandler handler)
+    {
+        String url = getApiUrl("list/movies/box_office.json");
+        RequestParams params = new RequestParams("apikey", API_KEY);
+        client.get(url, params, handler);
+    }
+}
